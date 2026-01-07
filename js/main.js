2301ef47ea62109d39b0acbb8cdef30fb1f255c2
Sandra KS <143679629+kssandraeshwar@users.noreply.github.com>
2026-01-08 04:17:52 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Typed text
+new Typed("#typed", {
+  strings: ["Sandra KS", "a Data Scientist", "an ML Researcher"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
+
+// Particles
+particlesJS.load('particles-js',
+  'https://raw.githubusercontent.com/VincentGarreau/particles.js/master/demo/particles.json'
+);
